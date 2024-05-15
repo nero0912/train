7922ea75c0c990a370593de4a97a6bb6d7c00e00
nero0912 <nerochen0912@gmail.com>
2024-05-15 16:38:16 +0800

    实现一个简单的注册接口

--- member/src/main/java/com/nero/train/member/controller/MemberController.java
+++ member/src/main/java/com/nero/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.nero.train.member.controller;
 import com.nero.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,11 @@ public class MemberController {
     @GetMapping("/count")
     public Integer count(){
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public long register(String mobile){
+        return memberService.register(mobile);
     }
 
 }
