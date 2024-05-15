f2d71cc98cb8563775b44478f3ac837b68a10419
nero0912 <nerochen0912@gmail.com>
2024-05-15 17:34:06 +0800

    使用自定义异常处理异常业务

--- common/src/main/java/com/nero/train/common/exception/BusinessException.java
+++ common/src/main/java/com/nero/train/common/exception/BusinessException.java
@@ -15,4 +15,10 @@ public class BusinessException extends RuntimeException {
     public void setE(BusinessExceptionEnum e) {
         this.e = e;
     }
+
+    /**
+     * 不写入堆栈信息，提高性能
+     */
+    @Override
+    public Throwable fillInStackTrace(){return this;}
 }
