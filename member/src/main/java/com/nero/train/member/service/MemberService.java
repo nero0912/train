d3fdec4d54948d1118d1f28c7ea827dbf0585d5f
nero0912 <nerochen0912@gmail.com>
2024-05-15 18:01:16 +0800

    使用雪花算法生成数据库id

--- member/src/main/java/com/nero/train/member/service/MemberService.java
+++ member/src/main/java/com/nero/train/member/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.nero.train.member.service;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.IdUtil;
 import com.nero.train.common.exception.BusinessException;
 import com.nero.train.common.exception.BusinessExceptionEnum;
+import com.nero.train.common.util.SnowUtil;
 import com.nero.train.member.domain.Member;
 import com.nero.train.member.domain.MemberExample;
 import com.nero.train.member.mapper.MemberMapper;
@@ -34,7 +36,7 @@ public class MemberService {
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
         Member member = new Member();
-        member.setId(System.currentTimeMillis());
+        member.setId(SnowUtil.getSnowflakeNextId());
         member.setMobile(mobile);
         memberMapper.insert(member);
         return member.getId();
